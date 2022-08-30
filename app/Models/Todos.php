68fb26b04aafabe29cd782f5f2ea3d023a73ea69
EsmerellEzra03 <esmerellezraensil@gmail.com>
2022-08-30 13:59:42 +0800

    Add Model for to-do

---
+++ app/Models/Todos.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Todos extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'date',
+    ];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
