b5774f2565e33bc7b470173bcd57bda47041bc8c
EsmerellEzra03 <esmerellezraensil@gmail.com>
2022-09-01 15:32:32 +0800

    change relationship in model

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
 
     public function todos()
     {
-        return $this->hasMany('App\Models\Todos');
+        return $this->hasOne('App\Models\Todos');
     }
 
     /**
